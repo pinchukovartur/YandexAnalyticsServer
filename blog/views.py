b878a255e4491ee2bddf61a8834da98c8ba13bb0
pinchukovartur <pinchukovartur@gmail.co>
2017-08-17 18:30:23 +0300

    add post controller

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.utils import timezone
+from django.shortcuts import redirect
+from .forms import PostForm
+from .models import Post
 
-# Create your views here.
+
+def post_list(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_list')
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
