e973fc712e3776d2c2aa72aed23aa8e30d2cd363
pinchukovartur <pinchukovartur@gmail.co>
2017-08-23 12:06:59 +0300

    fixed bug with output logs and added chance check old scripts

--- blog/media/scripts/number_of_installations.py
+++ blog/media/scripts/number_of_installations.py
@@ -1,5 +1,6 @@
 # внешняя библиотека MySQLdb (pip install MySQL-python)
 from MySQLdb import *
+import time
 
 """ Скрипт который выводит количество установок с 'даты-1'  по 'дату-2'"""
 
@@ -42,3 +43,5 @@ print("Количество записей - " + str(len(result)))
 print("Результат:")
 for row in result:
     print(row)
+
+time.sleep(520)
--- blog/scripts_controller/scripts_controller.py
+++ blog/scripts_controller/scripts_controller.py
@@ -1,9 +1,10 @@
 import os
 import xml.etree.cElementTree as ET
 from subprocess import Popen
-
+import datetime
 
 # install libs
+import psutil
 
 
 # метод запускающий скрипт
@@ -17,12 +18,14 @@ def run_script(script_name, script_type, auth_script, script_path, start_date_sc
         log_path = os.path.dirname(__file__) + "/logs/" + auth_script + "/"
         log_name = script_name + ".txt"
         __check_folder(log_path)
-        p = Popen("python " + script_path + ">" + log_path + log_name, shell=True)
+        p = Popen("python " + script_path + ">" + log_path + str(datetime.datetime.strftime(datetime.datetime.now(), "%Y.%m.%d_%H_%M_%S_")) + log_name, shell=True)
         print("python " + script_path + ">" + log_path + log_name)
         # записываем в конфиг данные скрипта
         config_path = os.path.dirname(__file__) + "/xml_data/"
         config_name = auth_script + ".xml"
 
+        print(p.pid)
+        print(os.getpid())
         _add_script_in_xml(config_path+config_name, script_name, script_type, start_date_script, auth_script, str(p.pid))
     else:
         raise NameError("ERROR!!! неизвестный тип скрипта")
@@ -66,25 +69,27 @@ def check_run_scripts(username):
 
 # метод удаляет из конфига все устаревшие скрипты
 def __delete_old_script_in_xml(config_path):
-    # проходим по всем активным pid-ам
-    # for process in psutil.process_iter():
+    # состояние скрипт по умолчанию не активное
+    state_script = False
+
     # проходим по всем скриптам в конфиге
     tree = ET.parse(config_path)
-    print(tree)
+    # проходим по всем активным pid-ам
+
     for scripts in tree.iter():
         if scripts.tag == "scripts":
             for script in scripts:
                 for subelem in script:
-                    print(subelem.tag)
-                    pid = subelem.tag
-                    if subelem.tag == "PID" and subelem.text == "7920":
+                    if subelem.tag == "PID":
+                        for process in psutil.process_iter():
+                            # проверям совпадает ли пид в конфиги с активными
+                            if str(subelem.text) == str(process.pid):
+                                # если такой пид есть, то делаем состояние активным
+                                state_script = True
+                            # если пид так и не был найдет, удаляем скрипт
+                if not state_script:
                         scripts.remove(script)
-                        pass
-                    #print(pid)
-
-                # tr.remove(script)
-
+    # записываем в файл
     tree.write(config_path)
 
-
-__delete_script_in_xml("D:\Projects\Django\web-post\\blog\scripts_controller\\xml_data\\admin.xml")
+__delete_old_script_in_xml("D:\Projects\Django\web-post\\blog\scripts_controller\\xml_data\\admin.xml")
